603f5e617588ae693870fc408dd15399f2ed0b97
Ka Yip Cheung <Kayip@detesters.nl>
2016-10-05 08:40:22 +0200

    added post request

--- src/test/scala/GatlingTest.scala
+++ src/test/scala/GatlingTest.scala
@@ -1,19 +1,28 @@
+package test.scala
+
 import io.gatling.http.Predef._
 import io.gatling.core.Predef._
 import scala.concurrent.duration._
 
 /**
- * Created by kayipcheung on 03/10/2016.
- */
+  * Created by kayipcheung on 03/10/2016.
+  */
 class GatlingTest extends Simulation {
-    val httpProtocol = http.baseURL("http://www.naamnummercontrole.nl").userAgentHeader("Naam Nummer Controle!");
+  val httpProtocol = http.baseURL("https://www.naamnummercontrole.nl").userAgentHeader("Naam Nummer Controle")
 
-    val myFirstScenario = scenario("Naar de Naam Nummer Control site")
-      .exec(
-        http("Open the naamnummercontrol start page")
-          .get("/")
-          .check(status.is(200))
+  val myFirstScenario = scenario("Check bank account")
+    .exec(
+      http("Open the Naam Nummer controle")
+        .get("/")
+        .check(status.is(200))
+    )
+    .pause(1)
+    .exec(
+      http("Perform a iban check action'")
+        .post("/api/iban/check/NAAMNUMMERCONTROLE%20BV/NL25INGB0007251387/z9qCFQ05ii")
+        .check(status.is(200))
     )
 
-    setUp(myFirstScenario.inject(rampUsers(1500) over (5 seconds)).protocols(httpProtocol))
+  setUp(
+    myFirstScenario.inject(rampUsers(10000) over (10 seconds)).protocols(httpProtocol))
 }
